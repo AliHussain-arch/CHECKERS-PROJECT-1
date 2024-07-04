0872e616ba5f518df04400c4dd68d953f12b1387
AliHussain <alihussain08950@gmail.com>
2024-07-04 11:38:15 +0300

    fourth commit the project files

---
+++ Checkers/script/script.js
@@ -0,0 +1,25 @@
+const active = document.querySelectorAll(".active");
+
+const board = [
+    [1,2,3,4],
+    [1,2,3,4],
+    [1,2,3,4],
+    [1,2,3,4],
+    [1,2,3,4],
+    [1,2,3,4],
+    [1,2,3,4],
+    [1,2,3,4]
+];
+
+//connecting the board to the array
+
+
+
+function legalMoves(element){
+    if(element.hasAtribute === '.row-1' && element.hasAtribute === '.element-1'){
+        active.
+    }
+}
+active.forEach(element => {
+    element.addEventListener('click', legalMoves);
+});
