4cb35f492ba56b0fe6eccaea202887ca6b71ee7a
AliHussain <alihussain08950@gmail.com>
2024-07-04 15:04:20 +0300

    fifth commit Board is setup and css is applied and 2 graphics mechanism added

--- Checkers/script/script.js
+++ Checkers/script/script.js
@@ -1,25 +1,61 @@
 const active = document.querySelectorAll(".active");
 
 const board = [
-    [1,2,3,4],
-    [1,2,3,4],
-    [1,2,3,4],
-    [1,2,3,4],
-    [1,2,3,4],
-    [1,2,3,4],
-    [1,2,3,4],
-    [1,2,3,4]
+    ['⚫','⚫','⚫','⚫'],
+    ['⚫','⚫','⚫','⚫'],
+    ['⚫','⚫','⚫','⚫'],
+    [0,0,0,0],
+    [0,0,0,0],
+    ['🔴','🔴','🔴','🔴'],
+    ['🔴','🔴','🔴','🔴'],
+    ['🔴','🔴','🔴','🔴'],
 ];
 
-//connecting the board to the array
+// Pieces clicked mechanism
 
+let clicked = false;
 
-
-function legalMoves(element){
-    if(element.hasAtribute === '.row-1' && element.hasAtribute === '.element-1'){
-        active.
+function click(event) {
+    const element = event.target;
+    if ((element.textContent === "🔴" || element.textContent === "⚫") && clicked !== true) {
+        element.classList.add("clicked");
+        clicked = true;
+    }
+    else if(element.textContent === "🔴" || element.textContent === "⚫") {
+        element.classList.remove("clicked");
+        clicked = false;
     }
 }
+
+// Highlighting on hover mechanism
+
+let hover = false;
+
+function piecesHover(event) {
+    const element = event.target;
+    if ((element.textContent === "🔴" || element.textContent === "⚫") && hover === false) {
+        element.classList.add("hoverOn");
+        hover = true;
+    }
+    else if (element.textContent === "🔴" || element.textContent === "⚫") {
+        element.classList.remove("hoverOn");
+        hover = false;
+    }
+}
+
+// Movement mechanism black pieces
+
+
+
+
+// Event listners collections
+
 active.forEach(element => {
-    element.addEventListener('click', legalMoves);
+    element.addEventListener('click', click);
+    element.addEventListener('mouseover', piecesHover);
+    element.addEventListener('mouseout', piecesHover);
 });
+
+
+
+
