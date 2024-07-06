7f57c085f9a1459f9422777b8e7caefe7339307d
AliHussain <alihussain08950@gmail.com>
2024-07-06 17:39:47 +0300

    coordinate system fixed and modified and rendering function created

--- Checkers/script/script.js
+++ Checkers/script/script.js
@@ -1,5 +1,21 @@
 const active = document.querySelectorAll(".active");
-
+const userBoard = document.querySelector("#boardtable");
+const row0 = document.querySelector('#row0'),
+      row0Cells = row0.querySelectorAll('td.active'),
+      row1 = document.querySelector('#row1'),
+      row1Cells = row1.querySelectorAll('td.active'),
+      row2 = document.querySelector('#row2'),
+      row2Cells = row2.querySelectorAll('td.active'),
+      row3 = document.querySelector('#row3'),
+      row3Cells = row3.querySelectorAll('td.active'),
+      row4 = document.querySelector('#row4'),
+      row4Cells = row4.querySelectorAll('td.active'),
+      row5 = document.querySelector('#row5'),
+      row5Cells = row5.querySelectorAll('td.active'),
+      row6 = document.querySelector('#row6'),
+      row6Cells = row6.querySelectorAll('td.active'),
+      row7 = document.querySelector('#row7'),
+      row7Cells = row7.querySelectorAll('td.active');
 const board = [
     ['⚫','⚫','⚫','⚫'],
     ['⚫','⚫','⚫','⚫'],
@@ -10,6 +26,25 @@ const board = [
     ['🔴','🔴','🔴','🔴'],
     ['🔴','🔴','🔴','🔴'],
 ];
+
+
+
+
+
+// rendering function
+function rendering(){
+    for(let i = 0;i<4;i++){
+        row0Cells[i].textContent=board[0][i];
+        row1Cells[i].textContent=board[1][i];
+        row2Cells[i].textContent=board[2][i];
+        row3Cells[i].textContent=board[3][i];
+        row4Cells[i].textContent=board[4][i];
+        row5Cells[i].textContent=board[5][i];
+        row6Cells[i].textContent=board[6][i];
+        row7Cells[i].textContent=board[7][i];
+    }
+}
+rendering();
 
 // Pieces clicked mechanism
 
